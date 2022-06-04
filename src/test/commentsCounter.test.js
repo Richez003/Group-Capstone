9286d196947743f99dcfafb05d3577990cb7b57e
richemonday@gmail.com <richemonday@gmail.com>
2022-06-04 16:08:15 +0100

    added coment conunter

---
+++ src/test/commentsCounter.test.js
@@ -0,0 +1,25 @@
+commentsCounter.test.js
+
+import commentsUrl from '../modules/commentsUrl.js';
+
+test('Display number of comments', () => {
+  const arr = [
+    {
+      comment: 'Nice Adventure Movie',
+      creation_date: '2022-05-19',
+      username: 'John',
+    },
+    {
+      username: 'Robert',
+      creation_date: '2022-05-19',
+      comment: 'Great Movie',
+    },
+    {
+      username: 'Forest Gump',
+      comment: 'Great Movie',
+      creation_date: '2022-05-19',
+    },
+  ];
+  const counter = commentsUrl.counterComments(arr);
+  expect(counter).toBe(3);
+});

b5595d2d0418fe983230b5aca2ee50e5185d2586
Stanmegga <ezeokestanley.ifeanyi@gmail.com>
2022-06-04 12:39:54 +0100

    Add display functionality to project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './style.css';
+import Movies from './modules/displayUrl.js';
+import fetchApi from './modules/fetchApi';
 
-import getApi from './modules/getApi';
+Movies.displayMovies();
+fetchApi.getLikes();
+Movies.counterMovies();

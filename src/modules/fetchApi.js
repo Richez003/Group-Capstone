074be6006fe75320f0d2426fa77547a727615bcf
Stanmegga <ezeokestanley.ifeanyi@gmail.com>
2022-06-04 16:02:02 +0100

    Add fetchApi functionality to project

---
+++ src/modules/fetchApi.js
@@ -0,0 +1,29 @@
+export default class fetchApi {
+  static likeUrl =
+    'https://us-central1-involvement-
+
+api.cloudfunctions.net/capstoneApi/apps/NU0JRPbStVeyNgQWILEC/lik
+
+es/';
+
+  static setLikes = async (id) => {
+    const response = await fetch(this.likeUrl, {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: id,
+      }),
+      headers: {
+        'Content-type': 'application/JSON',
+      },
+    });
+
+    const data = await response.text();
+    return data;
+  };
+
+  static getLikes = async () => {
+    const response = await fetch(this.likeUrl);
+    const data = await response.json();
+    return data;
+  };
+}
